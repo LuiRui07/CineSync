fa3dbf1731e401b7eaf7d94d111bbb99b7878159
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-12-18 23:57:53 +0100

    update

--- app/src/main/java/com/cinesync/Game.java
+++ app/src/main/java/com/cinesync/Game.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.sax.TextElementListener;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -13,9 +14,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +27,15 @@ import java.util.Random;
 
 public class Game extends AppCompatActivity {
     TextView categoriaText;
+
+    TextView nucleoText;
     DbHelper admin;
     String categoria;
     ImageButton ImgOp1,ImgOp2,ImgOp3,ImgOp4;
-
-    boolean respondida = false;
-
+    TextView TxOp1,TxOp2,TxOp3,TxOp4;
+    boolean tipoImagen = false;
+    boolean finCategoria = false;
+    Cursor fila;
     int rpCorrectaActual;
 
 
@@ -44,10 +51,17 @@ public class Game extends AppCompatActivity {
             ImgOp3 = findViewById(R.id.imageButton3);
             ImgOp4 = findViewById(R.id.imageButton4);
 
+            TxOp1 = findViewById(R.id.respuesta1);
+            TxOp2 = findViewById(R.id.respuesta2);
+            TxOp3 = findViewById(R.id.respuesta3);
+            TxOp4 = findViewById(R.id.respuesta4);
+
+            nucleoText = findViewById(R.id.nucleo);
             categoria = obtenerCategoria();
             categoriaText = findViewById(R.id.categoria);
             categoriaText.setText(categoria);
-            crearPreguntas(categoria);
+            crearCategoria(categoria);
+
     }
 
     public String obtenerCategoria() {
@@ -74,51 +88,114 @@ public class Game extends AppCompatActivity {
         }
     }
 
-    public void crearPreguntas(String cat){
+    public void crearCategoria(String cat){
         SQLiteDatabase bd = admin.getReadableDatabase();
         Cursor fila = bd.rawQuery("select nucleo,respuestasTexto,respuestasImagenes,respuestaCorrecta from preguntas where tag='"+cat+"'",null);
         if (fila.moveToFirst()) {
-           // do {
                 String nucleo = fila.getString(0);
                 String respustasTexto = fila.getString(1);
                 String respuestasImagenes = fila.getString(2);
                 int respuestaCorrecta = fila.getInt(3);
+                this.fila = fila;
                 crearPregunta(nucleo,respustasTexto,respuestasImagenes,respuestaCorrecta);
-           // } while (fila.moveToNext());
+        }
+    }
+
+    public void Siguiente(){
+        borrarTodo();
+        if (fila.moveToNext()){
+            String nucleo = fila.getString(0);
+            String respustasTexto = fila.getString(1);
+            String respuestasImagenes = fila.getString(2);
+            int respuestaCorrecta = fila.getInt(3);
+            crearPregunta(nucleo,respustasTexto,respuestasImagenes,respuestaCorrecta);
+        } else {
+            finCategoria = true;
         }
     }
 
     public void crearPregunta(String nuc, String rpTX, String rpImg, int rpC){
     //Aqui las creamos como tal 1 a 1.
         rpCorrectaActual = rpC;
+        nucleoText.setText(nuc);
         if (!TextUtils.isEmpty(rpImg)){ //Habria que tener en cuenta si hay 3 o 4 imagenes
+            tipoImagen = true;
             String[] enlacesArray = rpImg.split(",");
             ImageButton[] imageButtonsArray = {ImgOp1, ImgOp2, ImgOp3, ImgOp4};
-
             for (int i = 0; i < enlacesArray.length && i < imageButtonsArray.length; i++) {
                 String enlace = enlacesArray[i];
                 ImageButton imageButton = imageButtonsArray[i];
                 // Utiliza Picasso para cargar la imagen en el ImageButton
                 Picasso.get().load(enlace).into(imageButton);
             }
+        } else {
+            tipoImagen = false;
+            String[] respuestas = rpTX.split(",");
+            TextView[] respuestasTextArray = {TxOp1, TxOp2, TxOp3, TxOp4} ;
+            for (int i = 0; i < respuestas.length && i < respuestasTextArray.length; i++) {
+                String respuesta = respuestas[i];
+                TextView textView = respuestasTextArray[i];
+                textView.setText(respuesta);
+            }
         }
 
     }
 
-    public void pulsado(View view){
-        ImageButton imageButton = (ImageButton) view; //Aqui veo que boton ha pulsado
-        //Comprobar si es correcta o no
-        int buttonId = imageButton.getId();
+    public void borrarTodo(){
+        ImgOp1.setImageDrawable(null);
+        ImgOp2.setImageDrawable(null);
+        ImgOp3.setImageDrawable(null);
+        ImgOp4.setImageDrawable(null);
+        TxOp1.setText("");
+        TxOp2.setText("");
+        TxOp3.setText("");
+        TxOp4.setText("");
+    }
 
-        if (buttonId == R.id.imageButton1) {
-            Log.d("MiApp", "Se presionó el 1");
-        } else if (buttonId == R.id.imageButton2) {
-            Log.d("MiApp", "Se presionó el 2");
-        }else if (buttonId == R.id.imageButton3) {
-            Log.d("MiApp", "Se presionó el 3");
-        }else if (buttonId == R.id.imageButton4) {
-            Log.d("MiApp", "Se presionó el 4");
+    public void pulsado(View view){
+        int respuesta = -1;
+
+        if (tipoImagen){
+            ImageButton imageButton = (ImageButton) view; //Aqui veo que boton ha pulsado
+            //Comprobar si es correcta o no
+            int buttonId = imageButton.getId();
+            if (buttonId == R.id.imageButton1) {
+                Log.d("MiApp", "Se presionó el 1");
+                respuesta = 0;
+            } else if (buttonId == R.id.imageButton2) {
+                Log.d("MiApp", "Se presionó el 2");
+                respuesta = 1;
+            }else if (buttonId == R.id.imageButton3) {
+                Log.d("MiApp", "Se presionó el 3");
+                respuesta = 2;
+            }else if (buttonId == R.id.imageButton4) {
+                Log.d("MiApp", "Se presionó el 4");
+                respuesta = 3;
+            }
+        } else {
+            TextView textView = (TextView) view;
+            int textId = textView.getId();
+            if (textId == R.id.respuesta1) {
+                Log.d("MiApp", "Se presionó el 1");
+                respuesta = 0;
+            } else if (textId == R.id.respuesta2) {
+                Log.d("MiApp", "Se presionó el 2");
+                respuesta = 1;
+            }else if (textId == R.id.respuesta3) {
+                Log.d("MiApp", "Se presionó el 3");
+                respuesta = 2;
+            }else if (textId == R.id.respuesta4) {
+                Log.d("MiApp", "Se presionó el 4");
+                respuesta = 3;
+            }
         }
+
+        if (respuesta == rpCorrectaActual){
+            Toast.makeText(this,"Respuesta Correcta", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this,"Respuesta Incorrecta", Toast.LENGTH_SHORT).show();
+        }
+        Siguiente();
 
     }
 
